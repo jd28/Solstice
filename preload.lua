65b43694715c84c4dcd877c3501849874ed84694
jmd <jmd2028@gmail.com>
2012-04-07 23:11:04 -0700

    Fixed package path and directory structure

--- preload.lua
+++ preload.lua
@@ -1,13 +1,13 @@
-package.path = package.path .. ";./lua/?.lua"
+package.path = package.path .. ";./lua/?.lua;Solstice/?.lua"
 
 local lfs = require "lfs"
 require "lua.lua_preload"
 require "nwn.preload"
 
-for f in lfs.dir("./lua") do
+for f in lfs.dir("./Solstice") do
    if f ~= "preload.lua" and
       string.find(f:lower(), ".lua", -4) then
-      local file = lfs.currentdir() .. "/lua/" .. f
+      local file = lfs.currentdir() .. "/Solstice/" .. f
       print("Loading: " .. file)
       dofile(file)
    end
