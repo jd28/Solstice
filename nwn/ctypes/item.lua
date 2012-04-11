76c7ba754f4973c1098e52e43bda1963b623eece
jmd <jmd2028@gmail.com>
2012-04-11 12:14:25 -0700

    Added CNWBaseItem struct

--- nwn/ctypes/item.lua
+++ nwn/ctypes/item.lua
@@ -19,6 +19,83 @@
 local ffi = require 'ffi'
 
 ffi.cdef[[
+typedef struct CNWBaseItem_s {
+        uint32_t        bi_name_strref;                         /* 0x000 */
+        uint32_t        bi_equip_slots;                         /* 0x004 */
+        uint8_t         bi_weapon_wield;                        /* 0x008 */
+        uint8_t         bi_damage_type;                         /* 0x009 */
+        uint8_t         field_A;                                /* 0x00A */
+        uint8_t         field_B;                                /* 0x00B */
+        uint32_t        MinRange;                               /* 0x00C */
+        uint32_t        MaxRange;                               /* 0x0010 */
+        uint8_t         InvSlotWidth;                   /* 0x0014 */
+        uint8_t         InvSlotHeight;                  /* 0x0015 */
+        uint8_t         bi_model_type;                          /* 0x0016 */
+        uint8_t         ChargesStarting;                /* 0x0017 */
+        uint8_t         bi_is_ranged;                           /* 0x0018 */
+        uint8_t         bi_size;                                /* 0x0019 */
+        uint8_t         bi_dmg_dice;                            /* 0x001A */
+        uint8_t         bi_dmg_sides;                           /* 0x001B */
+        uint8_t         bi_crit_range;                          /* 0x001C */
+        uint8_t         bi_crit_mult;                           /* 0x001D */
+        uint8_t         Category;                               /* 0x001E */
+        uint8_t         field_1F;                               /* 0x001F */
+        float           BaseCost;                               /* 0x0020 */
+        uint8_t         Stacking;                               /* 0x0024 */
+        uint8_t         field_25;                               /* 0x0025 */
+        uint8_t         field_26;                               /* 0x0026 */
+        uint8_t         field_27;                               /* 0x0027 */
+        uint8_t         ItemMultiplier;                 /* 0x0028 */
+        uint8_t         field_29;                               /* 0x0029 */
+        uint8_t         field_2A;                               /* 0x002A */
+        uint8_t         field_2B;                               /* 0x002B */
+        uint32_t        bi_desc_strref;                         /* 0x002C */
+        uint8_t         MinProps;                               /* 0x0030 */
+        uint8_t         MaxProps;                               /* 0x0031 */
+        uint8_t         PropColumn;                             /* 0x0032 */
+        uint8_t         StorePanel;                             /* 0x0033 */
+        uint8_t         StorePanelSort;                 /* 0x0034 */
+        uint8_t         AnimSlashL;                             /* 0x0035 */
+        uint8_t         AnimSlashR;                             /* 0x0036 */
+        uint8_t         AnimSlashS;                             /* 0x0037 */
+        uint8_t         bi_ilr_stack_size;                      /* 0x0038 */
+        uint8_t         field_39;                               /* 0x0039 */
+        uint8_t         field_3A;                               /* 0x003A */
+        uint8_t         field_3B;                               /* 0x003B */
+        float           PrefAttackDist;                 /* 0x003C */
+        char            ItemClass[16];                  /* 0x0040 */
+        uint32_t        CanRotateIcon;                  /* 0x0050 */
+        uint32_t        field_54;                               /* 0x0054 */
+        char            DefaultIcon[16];                /* 0x0058 */
+        char            DefaultModel[16];               /* 0x0068 */
+        uint8_t         field_78;                       /* 0x0078 */
+        uint8_t         field_79;                       /* 0x0079 */
+        uint8_t         field_7A;                       /* 0x007A */
+        uint8_t         field_7B;                               /* 0x007B */
+        uint8_t         Container;                              /* 0x007C */
+        uint8_t         field_7D;                               /* 0x007D */
+        uint8_t         field_7E;                               /* 0x007E */
+        uint8_t         field_7F;                               /* 0x007F */
+        uint8_t         field_80;                               /* 0x0080 */
+        uint8_t         field_81;                               /* 0x0081 */
+        uint8_t         field_82;                               /* 0x0082 */
+        uint8_t         field_83;                               /* 0x0083 */
+        uint32_t        InventorySoundType;             /* 0x0084 */
+        uint32_t       *bi_req_feats;                           /* 0x0088 */
+        uint32_t        bi_req_feats_len;                       /* 0x008C */
+        uint32_t        bi_basic_stat_strref;                   /* 0x0090 */
+        uint32_t        RotateOnGround;                 /* 0x0094 */
+        uint32_t        bi_ten_times_lbs;                       /* 0x0098 */
+        uint8_t         bi_ac_base;                             /* 0x009C */
+        uint8_t         bi_ac_type;                             /* 0x009D */
+        uint8_t         bi_weapon_sound;                        /* 0x009E */
+        int8_t          bi_armor_check_penaly;                  /* 0x009F */
+        uint8_t         bi_ammunition_type;                     /* 0x00A0 */
+        uint8_t         QBBehaviour;                    /* 0x00A1 */
+        uint8_t         bi_arcane_spell_fail;                   /* 0x00A2 */
+        uint8_t         field_A3;                               /* 0x00A3 */
+} CNWBaseItem;
+
 typedef struct CNWSItem {
     uint8_t                     it_color[6];            /* 0000 */
     uint8_t                     field_06;
