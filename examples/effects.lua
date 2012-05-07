0ea5b4fd11d767eb913cae5409c9cc97bfb38af5
jmd <jmd2028@gmail.com>
2012-05-07 01:35:06 -0700

    Renamed example effects file Updated to new custom effect functions.

--- examples/effects.lua
+++ examples/effects.lua
@@ -1,6 +1,6 @@
 require "nwn.object"
 require "nwn.effects"
-local nie = require 'nwn.internal.effects'
+require 'nwn.effects'
 
 TA_EFFECT_DECREASE_DC = 3
 
@@ -9,7 +9,7 @@ function nwn.EffectDecreaseDC(amount)
    return eff
 end
 
-nie.RegisterCustomEffect(TA_EFFECT_DECREASE_DC,
+nwn.RegisterCustomEffect(TA_EFFECT_DECREASE_DC,
     function (effect, target, is_apply)
        local amount = effect:GetInt(1)
        if is_apply then
