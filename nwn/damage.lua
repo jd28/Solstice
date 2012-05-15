bfa439960c3a9b448db9bb890a1f9503db541a71
jmd <jmd2028@gmail.com>
2012-05-15 02:59:30 -0700

    Damage registration extension
    Extended damage type registration to include
    ranged visual effects and IP_CONST_DAMAGETYPE_*
    constants.
    
    Added function for determining a base damage type
    from an IP_CONST.

--- nwn/damage.lua
+++ nwn/damage.lua
@@ -24,16 +24,20 @@ require 'nwn.dice'
 DAMAGES.roll = {}
 DAMAGES.index = {}
 DAMAGES.format = {}
+DAMAGES.ip_type = {}
 
 --- Register Damage Types.
 -- This function MUST only be called from OnModuleLoad!
 -- @param damage_type nwn.DAMAGE_TYPE_*
-function nwn.RegisterDamage(damage_type, index, name, dmg_color, melee_vfx)
+function nwn.RegisterDamage(damage_type, index, name, dmg_color, melee_vfx, ranged_vfx, ip_type)
    table.insert(DAMAGES, damage_type)
    DAMAGES[damage_type] = { name = name,
                             color = dmg_color,
-                            melee_vfx = melee_vfx
+                            melee_vfx = melee_vfx,
+			    ranged_vfx = ranged_vfx,
+			    ip_type = ip_type
    }
+   DAMAGES.ip_type[ip_type] = damage_type
    DAMAGES.index[damage_type] = index
    DAMAGES.format[index] = dmg_color .. "%d " .. name  .. color.END
 end
@@ -52,6 +56,11 @@ function nwn.RegisterDamageRoll(id, dice, side, bonus)
 --   print(id)
 
    DAMAGES.roll[id] = dice_roll_t(dice, side, bonus)
+end
+
+--- Get damage flag from IP Constant
+function nwn.GetDamageFlagFromIPConst(ip_type)
+   return DAMAGES.ip_type[ip_type] or -1
 end
 
 --- Get damage format by index
@@ -90,19 +99,19 @@ end
 
 -- nwn.RegisterDamage registers a particular damage type, its index, name, color, and color.
 -- all combat damage formating will take care of itself.
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_BLUDGEONING, 0, "Bludgeoning", color.ORANGE)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_PIERCING, 1, "Piercing", color.ORANGE)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_SLASHING, 2, "Slashing", color.ORANGE)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_MAGICAL, 3, "Magical", color.PURPLE, 76)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_ACID, 4, "Acid", color.GREEN, 283)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_COLD, 5, "Cold", color.LIGHT_BLUE, 281)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_DIVINE, 6, "Divine", color.YELLOW, 289)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_ELECTRICAL, 7, "Electrical", color.DARK_BLUE, 282)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_FIRE, 8, "Fire", color.RED, 280)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_NEGATIVE, 9, "Negative", color.GRAY, 282)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_POSITIVE, 10, "Positive", color.WHITE, 289)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_SONIC, 11, "Sonic", color.LIGHT_ORANGE, 284)
-nwn.RegisterDamage(nwn.DAMAGE_TYPE_BASE_WEAPON, 12, "Physical", color.ORANGE)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_BLUDGEONING, 0, "Bludgeoning", color.ORANGE, nil, nil, nwn.IP_CONST_DAMAGETYPE_BLUDGEONING)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_PIERCING, 1, "Piercing", color.ORANGE, nil, nil, nwn.IP_CONST_DAMAGETYPE_PIERCING)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_SLASHING, 2, "Slashing", color.ORANGE, nil, nil, nwn.IP_CONST_DAMAGETYPE_SLASHING)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_MAGICAL, 3, "Magical", color.PURPLE, 76, nil, nwn.IP_CONST_DAMAGETYPE_MAGICAL)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_ACID, 4, "Acid", color.GREEN, 283, nil, nwn.IP_CONST_DAMAGETYPE_ACID)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_COLD, 5, "Cold", color.LIGHT_BLUE, 281, nil, nwn.IP_CONST_DAMAGETYPE_COLD)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_DIVINE, 6, "Divine", color.YELLOW, 289, nil, nwn.IP_CONST_DAMAGETYPE_DIVINE)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_ELECTRICAL, 7, "Electrical", color.DARK_BLUE, 282, nil, nwn.IP_CONST_DAMAGETYPE_ELECTRICAL)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_FIRE, 8, "Fire", color.RED, 280, nil, nwn.IP_CONST_DAMAGETYPE_FIRE)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_NEGATIVE, 9, "Negative", color.GRAY, 282, nil, nwn.IP_CONST_DAMAGETYPE_NEGATIVE)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_POSITIVE, 10, "Positive", color.WHITE, 289, nil, nwn.IP_CONST_DAMAGETYPE_POSITIVE)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_SONIC, 11, "Sonic", color.LIGHT_ORANGE, 284, nil, nwn.IP_CONST_DAMAGETYPE_SONIC)
+nwn.RegisterDamage(nwn.DAMAGE_TYPE_BASE_WEAPON, 12, "Physical", color.ORANGE, nil, nil, nwn.IP_CONST_DAMAGETYPE_PHYSICAL)
 
 return DAMAGES
 
