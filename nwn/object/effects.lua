62460da5f4ec9f3b1c4a88fc628a9c1236b53ea8
jmd <jmd2028@gmail.com>
2012-07-02 04:33:53 -0700

    nwn/object/effects.lua added functions
    -- Added function to determine number of effects applied to an
       object.
    -- Added a function to get an effect at a specified index in an
       objects effect array.

--- nwn/object/effects.lua
+++ nwn/object/effects.lua
@@ -1,21 +1,3 @@
---------------------------------------------------------------------------------
---  Copyright (C) 2011-2012 jmd ( jmd2028 at gmail dot com )
--- 
---  This program is free software; you can redistribute it and/or modify
---  it under the terms of the GNU General Public License as published by
---  the Free Software Foundation; either version 2 of the License, or
---  (at your option) any later version.
---
---  This program is distributed in the hope that it will be useful,
---  but WITHOUT ANY WARRANTY; without even the implied warranty of
---  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
---  GNU General Public License for more details.
---
---  You should have received a copy of the GNU General Public License
---  along with this program; if not, write to the Free Software
---  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
---------------------------------------------------------------------------------
-
 local ffi = require 'ffi'
 local C = ffi.C
 
@@ -120,6 +102,18 @@ function Object:EffectsDirect()
          return effect_t(obj.obj_effects[_i], true)
       end
    end
+end
+
+function Object:GetEffectAtIndex(idx)
+   local obj = self.obj.obj
+   if idx < 0 or idx >= obj.obj_effects_len then
+      return
+   end
+   return effect_t(obj.obj_effects[idx], true)
+end
+
+function Object:GetEffectCount()
+   return self.obj.obj.obj_effects_len - 1
 end
 
 --- Get if an object has an effecy by ID
