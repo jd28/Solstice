1dc90c910f6326e85c06f79699d6c1a6ac5dd573
jmd <jmd2028@gmail.com>
2012-07-02 04:30:49 -0700

    Added function cache for stack commands.

--- nwn/module.lua
+++ nwn/module.lua
@@ -1,25 +1,12 @@
---------------------------------------------------------------------------------
---  Copyright (C) 2011-2012 jmd ( jmd2028 at gmail dot com )
--- 
---  This program is free software; you can redistribute it and/or modify
---  it under the terms of the GNU General Public License as published by
---  the Free Software Foundation; either version 2 of the License, or
---  (at your option) any later version.
---
---  This program is distributed in the hope that it will be useful,
---  but WITHOUT ANY WARRANTY; without even the implied warranty of
---  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
---  GNU General Public License for more details.
---
---  You should have received a copy of the GNU General Public License
---  along with this program; if not, write to the Free Software
---  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
---------------------------------------------------------------------------------
-
 require "nwn.engine"
 require 'nwn.ctypes.module'
 require "nwn.module.settings"
 local ffi = require 'ffi'
+
+local ExecuteCommand = nwn.engine.ExecuteCommand
+local StackPopEngineStructure = nwn.engine.StackPopEngineStructure
+local StackPopString = nwn.engine.StackPopString
+
 
 ffi.cdef [[
 typedef struct Module {
@@ -45,13 +32,13 @@ end
 
 --- Get module name
 function Module:GetName()
-   nwn.engine.ExecuteCommand(561, 0)
+   ExecuteCommand(561, 0)
    return StackPopString()
 end
 
 --- Get module starting location
 function Module:GetStartingLocation()
-   nwn.engine.ExecuteCommand(411, 0)
+   ExecuteCommand(411, 0)
    return StackPopEngineStructure(nwn.ENGINE_STRUCTURE_LOCATION)
 end
 
