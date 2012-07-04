c1854dfc728a7d03a5cb001ab62e280a5efd749d
jmd <jmd2028@gmail.com>
2012-07-04 04:17:26 -0700

    Split effect/stacking policies into new file

---
+++ examples/stacking_polcies.lua
@@ -0,0 +1,85 @@
+-- Ability
+
+ABILITY_EFF_INFO = 
+   effect_info_t(nwn.EFFECT_TRUETYPE_ABILITY_DECREASE,
+		 nwn.EFFECT_TRUETYPE_ABILITY_INCREASE,
+		 true,
+		 false,
+		 false)
+
+
+--------------------------------------------------------------------------------
+-- Attack Bonus
+
+AB_EFF_INFO = 
+   effect_info_t(nwn.EFFECT_TRUETYPE_ATTACK_DECREASE,
+		 nwn.EFFECT_TRUETYPE_ATTACK_INCREASE,
+		 true,
+		 false,
+		 false)
+
+--------------------------------------------------------------------------------
+-- Armor Class
+
+AC_ARMOR_EFF_INFO = 
+   effect_info_t(nwn.EFFECT_TRUETYPE_AC_DECREASE,
+		 nwn.EFFECT_TRUETYPE_AC_INCREASE,
+		 false,
+		 false,
+		 false)
+
+AC_DEFLECTION_EFF_INFO =
+   effect_info_t(nwn.EFFECT_TRUETYPE_AC_DECREASE,
+		 nwn.EFFECT_TRUETYPE_AC_INCREASE,
+		 false,
+		 false,
+		 false)
+
+AC_DODGE_EFF_INFO = 
+   effect_info_t(nwn.EFFECT_TRUETYPE_AC_DECREASE,
+		 nwn.EFFECT_TRUETYPE_AC_INCREASE,
+		 true, 
+		 false, 
+		 false)
+
+AC_NATURAL_EFF_INFO = 
+   effect_info_t(nwn.EFFECT_TRUETYPE_AC_DECREASE,
+		 nwn.EFFECT_TRUETYPE_AC_INCREASE,
+		 false,
+		 false,
+		 false)
+
+AC_SHIELD_EFF_INFO = 
+   effect_info_t(nwn.EFFECT_TRUETYPE_AC_DECREASE,
+		 nwn.EFFECT_TRUETYPE_AC_INCREASE,
+		 false,
+		 false,
+		 false)
+
+--------------------------------------------------------------------------------
+-- Damage
+
+DAMAGE_EFF_INFO = 
+   effect_info_t(nwn.EFFECT_TRUETYPE_DAMAGE_DECREASE,
+		 nwn.EFFECT_TRUETYPE_DAMAGE_INCREASE,
+		 true,
+		 true,
+		 false)
+
+-- Saves
+SAVE_EFF_INFO = 
+   effect_info_t(nwn.EFFECT_TRUETYPE_SAVING_THROW_DECREASE,
+		 nwn.EFFECT_TRUETYPE_SAVING_THROW_INCREASE,
+		 true,
+		 false,
+		 false)
+
+
+-- Skills
+
+SKILL_EFF_INFO = 
+   effect_info_t(nwn.EFFECT_TRUETYPE_SKILL_DECREASE,
+		 nwn.EFFECT_TRUETYPE_SKILL_INCREASE,
+		 true,
+		 false,
+		 false)
