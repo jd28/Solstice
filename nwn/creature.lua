e0a76060d448ab7be04ee04573db26d817f3cd8a
jmd <jmd2028@gmail.com>
2012-07-04 04:21:09 -0700

    Expand nwn/creature.lua
    -- Added more requires.
    -- Expanded CombatWeapon struct.

--- nwn/creature.lua
+++ nwn/creature.lua
@@ -1,21 +1,3 @@
---------------------------------------------------------------------------------
---  Copyright (C) 2011-2012 jmd ( jmd2028 at gmail dot com )
--- 
---  This program is free software; you can redistribute it and/or modify
---  it under the terms of the GNU General Public License as published by
---  the Free Software Foundation; either version 2 of the License, or
---  (at your option) any later version.
---
---  This program is distributed in the hope that it will be useful,
---  but WITHOUT ANY WARRANTY; without even the implied warranty of
---  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
---  GNU General Public License for more details.
---
---  You should have received a copy of the GNU General Public License
---  along with this program; if not, write to the Free Software
---  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
---------------------------------------------------------------------------------
-
 nwn.SITUATIONAL_FLAG_COUP_DE_GRACE = 1
 nwn.SITUATIONAL_FLAG_SNEAK_ATTACK = 2
 nwn.SITUATIONAL_FLAG_DEATH_ATTACK = 4
@@ -42,7 +24,8 @@ typedef struct CombatWeapon {
    uint8_t crit_range;
    uint8_t crit_mult;
    uint16_t slot;
-
+   uint16_t base_type;
+   uint16_t base_mask;
    DiceRoll base_dmg;
    DiceRoll crit_dmg;
 } CombatWeapon;
@@ -112,26 +95,30 @@ require 'nwn.creature.ability'
 require 'nwn.creature.action'
 require 'nwn.creature.ai'
 require 'nwn.creature.alignment'
+require 'nwn.creature.armor_class'
 require 'nwn.creature.associate'
+require 'nwn.creature.attack_bonus'
 require 'nwn.creature.class'
 require 'nwn.creature.combat'
 require 'nwn.creature.cutscene'
+require 'nwn.creature.damage'
 require 'nwn.creature.effects'
 require 'nwn.creature.faction'
 require 'nwn.creature.feats'
+require 'nwn.creature.hp'
 require 'nwn.creature.info'
 require 'nwn.creature.internal'
 require 'nwn.creature.inventory'
 require 'nwn.creature.logger'
 require 'nwn.creature.modes'
 require 'nwn.creature.pc'
+require 'nwn.creature.saves'
 require 'nwn.creature.skills'
 require 'nwn.creature.spells'
 require 'nwn.creature.state'
 require 'nwn.creature.talent'
 require 'nwn.creature.xp'
-require 'nwn.creature.hp'
-require 'nwn.creature.saves'
+
 
 --- Briefly displays a string ref as ambient text above targets head.
 -- @param strref String ref (therefore text is translated)
