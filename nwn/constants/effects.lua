1426124c9765a4e27f5cf15c379bdc740429a941
jmd <jmd2028@gmail.com>
2012-07-08 02:20:15 -0700

    Removed spurious constant.

--- nwn/constants/effects.lua
+++ nwn/constants/effects.lua
@@ -8,7 +8,6 @@ nwn.DURATION_TYPE_INNATE     = 4
 nwn.SUBTYPE_MAGICAL          = 8
 nwn.SUBTYPE_SUPERNATURAL     = 0x10
 nwn.SUBTYPE_EXTRAORDINARY    = 0x18
-nwn.SUBTYPE_UNRESISTABLE     = 0x20
 
 nwn.EFFECT_TRUETYPE_INVALIDEFFECT                         =   0
 nwn.EFFECT_TRUETYPE_HASTE                                 =   1
