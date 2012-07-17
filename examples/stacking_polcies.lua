dad8b8d95c2f74ed9d69c769582a3e7dbebc0658
jmd <jmd2028@gmail.com>
2012-07-17 01:38:16 -0700

    Reformatting, Added effect info for HP effects

--- examples/stacking_polcies.lua
+++ examples/stacking_polcies.lua
@@ -3,9 +3,7 @@
 ABILITY_EFF_INFO = 
    effect_info_t(nwn.EFFECT_TRUETYPE_ABILITY_DECREASE,
 		 nwn.EFFECT_TRUETYPE_ABILITY_INCREASE,
-		 true,
-		 false,
-		 false)
+		 true, false, false)
 
 
 --------------------------------------------------------------------------------
@@ -14,9 +12,7 @@ ABILITY_EFF_INFO =
 AB_EFF_INFO = 
    effect_info_t(nwn.EFFECT_TRUETYPE_ATTACK_DECREASE,
 		 nwn.EFFECT_TRUETYPE_ATTACK_INCREASE,
-		 true,
-		 false,
-		 false)
+		 true, false, false)
 
 --------------------------------------------------------------------------------
 -- Armor Class
@@ -24,37 +20,35 @@ AB_EFF_INFO =
 AC_ARMOR_EFF_INFO = 
    effect_info_t(nwn.EFFECT_TRUETYPE_AC_DECREASE,
 		 nwn.EFFECT_TRUETYPE_AC_INCREASE,
-		 false,
-		 false,
-		 false)
+		 false, false, false)
 
 AC_DEFLECTION_EFF_INFO =
    effect_info_t(nwn.EFFECT_TRUETYPE_AC_DECREASE,
 		 nwn.EFFECT_TRUETYPE_AC_INCREASE,
-		 false,
-		 false,
-		 false)
+		 false, false, false)
 
 AC_DODGE_EFF_INFO = 
    effect_info_t(nwn.EFFECT_TRUETYPE_AC_DECREASE,
 		 nwn.EFFECT_TRUETYPE_AC_INCREASE,
-		 true, 
-		 false, 
-		 false)
+		 true, false, false)
 
 AC_NATURAL_EFF_INFO = 
    effect_info_t(nwn.EFFECT_TRUETYPE_AC_DECREASE,
 		 nwn.EFFECT_TRUETYPE_AC_INCREASE,
-		 false,
-		 false,
-		 false)
+		 false, false, false)
 
 AC_SHIELD_EFF_INFO = 
    effect_info_t(nwn.EFFECT_TRUETYPE_AC_DECREASE,
 		 nwn.EFFECT_TRUETYPE_AC_INCREASE,
-		 false,
-		 false,
-		 false)
+		 false, false, false)
+
+--------------------------------------------------------------------------------
+-- Hit points
+
+HP_EFF_INFO = 
+   effect_info_t(nwn.EFFECT_CUSTOMTYPE_HP_DECREASE,
+		 nwn.EFFECT_CUSTOMTYPE_HP_INCREASE,
+		 true, false, false)
 
 --------------------------------------------------------------------------------
 -- Damage
@@ -62,24 +56,17 @@ AC_SHIELD_EFF_INFO =
 DAMAGE_EFF_INFO = 
    effect_info_t(nwn.EFFECT_TRUETYPE_DAMAGE_DECREASE,
 		 nwn.EFFECT_TRUETYPE_DAMAGE_INCREASE,
-		 true,
-		 true,
-		 false)
+		 true, true, false)
 
 -- Saves
 SAVE_EFF_INFO = 
    effect_info_t(nwn.EFFECT_TRUETYPE_SAVING_THROW_DECREASE,
 		 nwn.EFFECT_TRUETYPE_SAVING_THROW_INCREASE,
-		 true,
-		 false,
-		 false)
+		 true, false, false)
 
 
 -- Skills
-
 SKILL_EFF_INFO = 
    effect_info_t(nwn.EFFECT_TRUETYPE_SKILL_DECREASE,
 		 nwn.EFFECT_TRUETYPE_SKILL_INCREASE,
-		 true,
-		 false,
-		 false)
+		 true, false, false)
