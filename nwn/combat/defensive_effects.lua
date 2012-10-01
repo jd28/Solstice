3837d60f74da0284278539a407f6d893570b2a15
jmd <jmd2028@gmail.com>
2012-10-01 01:16:52 -0700

    Fix concealment / miss chance.

--- nwn/combat/defensive_effects.lua
+++ nwn/combat/defensive_effects.lua
@@ -1,25 +1,13 @@
---------------------------------------------------------------------------------
---  Copyright (C) 2011-2012 jmd ( jmd2028 at gmail dot com )
--- 
---  This program is free software; you can redistribute it and/or modify
---  it under the terms of the GNU General Public License as published by
---  the Free Software Foundation; either version 2 of the License, or
---  (at your option) any later version.
---
---  This program is distributed in the hope that it will be useful,
---  but WITHOUT ANY WARRANTY; without even the implied warranty of
---  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
---  GNU General Public License for more details.
---
---  You should have received a copy of the GNU General Public License
---  along with this program; if not, write to the Free Software
---  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
---------------------------------------------------------------------------------y
-
 local ffi = require 'ffi'
 local C = ffi.C
 local bit = require 'bit'
+local random = math.random
 
+--- Determine miss chance / concealment.
+-- @param attacker Attacker
+-- @param target Target
+-- @return Returns true if the attacker failed to over come miss chance
+--    or concealment.
 function NSResolveMissChance(attacker, target, hit, attack_info)
    if target.type ~= nwn.GAME_OBJECT_TYPE_CREATURE then
       return false
@@ -30,28 +18,34 @@ function NSResolveMissChance(attacker, target, hit, attack_info)
    -- Conceal
    local conceal = target:GetConcealment(attacker, attack_info.attack)
 
-   if conceal > 0 or miss_chance > 0 then
-      if miss_chance < conceal then
-         if math.random(1, 100) >= conceal
-            or (attacker:GetHasFeat(nwn.FEAT_BLIND_FIGHT)
-                and math.random(1, 100) >= conceal)
-         then
-            attack_info.attack.cad_attack_result = 8
-            attack_info.attack.cad_concealment = math.floor((conceal ^ 2) / 100)
-            attack_info.attack.cad_missed = 1
-            return true
-         end
-      else
-         if math.random(1, 100) >= miss_chance
-            or (attacker:GetHasFeat(nwn.FEAT_BLIND_FIGHT)
-                and math.random(1, 100) >= miss_chance)
-         then
-            attack_info.attack.cad_attack_result = 9
-            attack_info.attack.cad_concealment = math.floor((miss_chance ^ 2) / 100)
-            attack_info.attack.cad_missed = 1
-            return true
-         end
-      end
+   -- If concealment and mis-chance are less than or equal to zero
+   -- there is no chance of them affecting the outcome of an attack.
+   if conceal <= 0 and miss_chance <= 0 then return false end
+
+   local chance, attack_result
+
+   -- Deterimine which of miss chance and concealment is higher.
+   -- attack_result is a magic number for the NWN combat engine that
+   -- determines which combat messages are sent to the player.
+   if miss_chance < conceal then
+      chance = conceal
+      attack_result = 8
+   else
+      chance = miss_chance
+      attack_result = 9
    end
-   return false
+
+   -- The attacker has two possible chances to over come miss chance / concealment
+   -- if they posses the blind fight feat.  If not they only have one chance to do so.
+   if random(100) >= chance
+      or (attacker:GetHasFeat(nwn.FEAT_BLIND_FIGHT) and random(100) >= chance)
+   then
+      return false
+   else
+      attack_info.attack.cad_attack_result = attack_result
+      -- Show the modified conceal/miss chance in the combat log.
+      attack_info.attack.cad_concealment = math.floor((chance ^ 2) / 100)
+      attack_info.attack.cad_missed = 1
+      return true
+   end
 end
