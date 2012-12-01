903ea57492fdb37fb70f3a3b5d7de7a6538faf86
jmd <jmd2028@gmail.com>
2012-12-01 00:26:34 -0800

    nwn/spells Add file for spell functions
    -- Add just a couple helper functions.

---
+++ nwn/spells.lua
@@ -0,0 +1,23 @@
+--- Gets a random delay
+-- @param min Minimum delay
+-- @param max Maximum delay
+function nwn.GetRandomDelay(min, max)
+   min = min or 0.4
+   max = max or 1.1
+   local rand = max - min
+   if rand < 0 then
+      return 0
+   end
+   rand = rand  * 10.0
+   rand = math.random(rand)
+
+   return rand / 10 + min
+end
+
+--- Determines a delay for spell effects
+--    Distance between location and object divided by 20
+-- @param location Location of spell impact/caster
+-- @param target Spell target
+function nwn.GetSpellEffectDelay(location, target)
+   return location:GetDistanceBetween(target:GetLocation()) / 20
+end
