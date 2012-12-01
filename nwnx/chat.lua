2a79dd6fc921be4430589949a14ed14b97056c05
jmd <jmd2028@gmail.com>
2012-12-01 00:00:46 -0800

    nwnx/chat Add CC message handler, remove mod specifics
    -- Remove module specific code.
    -- Add functions for setting a CC message handler.

--- nwnx/chat.lua
+++ nwnx/chat.lua
@@ -2,6 +2,7 @@ local ffi = require 'ffi'
 local C = ffi.C
 
 local CHAT_HANDLER
+local CC_HANDLER
 
 NWNXChat = {}
 
@@ -10,20 +11,34 @@ function NWNXChat.SetChatHandler(func)
    CHAT_HANDLER = func
 end
 
+function NWNXChat.SetCombatMessageHandler(func)
+   CC_HANDLER = func
+end
+
+
 function NWNXChat_HandleChatMessage()
    if not CHAT_HANDLER then return false end
 
    local msg = C.Local_GetLastChatMessage()
+   if msg == nil then 
+      error "NWNXChat_HandleChatMessage : NULL msg!"
+   end
+
+
    msg.suppress = CHAT_HANDLER(msg.channel, _NL_GET_CACHED_OBJECT(msg.from),
 			       ffi.string(msg.msg), _NL_GET_CACHED_OBJECT(msg.to))
-   return true
+   return msg.suppress
 end
 
 function NWNXChat_HandleCombatMessage()
+   if not CHAT_HANDLER then return false end
+
    local msg = C.Local_GetLastCombatMessage()
-   if msg.type == 151 then
-      return true
+   if msg == nil then 
+      error "NWNXChat_HandleCombatMessage : NULL msg!"
    end
+
+   msg.suppress = CC_HANDLER(msg)
    
-   return false
+   return msg.suppress
 end
