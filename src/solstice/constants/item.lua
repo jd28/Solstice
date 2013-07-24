9161e9131891c6a4147c939530e001a23822aa3e
jmd <jmd2028@gmail.com>
2013-07-24 03:37:12 -0700

    item: Merge & remove obsolete constant file.

--- src/solstice/constants/item.lua
+++
@@ -1,8 +0,0 @@
-
-solstice.RANGED_TYPE_NONE      = 0
-solstice.RANGED_TYPE_BOW       = 1
-solstice.RANGED_TYPE_SLING     = 2
-solstice.RANGED_TYPE_CROSSBOW  = 3
-solstice.RANGED_TYPE_SHURIKEN  = 4
-solstice.RANGED_TYPE_DART      = 5
-solstice.RANGED_TYPE_THROWAXE  = 6
