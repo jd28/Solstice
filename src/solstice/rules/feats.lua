d910136f981a64078905460cc0a272060db24477
jmd <jmd2028@gmail.com>
2015-01-02 04:13:27 -0800

    rules.feats: Add more functions.

--- src/solstice/rules/feats.lua
+++ src/solstice/rules/feats.lua
@@ -54,3 +54,39 @@ function NWNXSolstice_GetMaximumFeatUses(feat, cre)
    cre = _SOL_GET_CACHED_OBJECT(cre)
    return M.GetMaximumFeatUses(feat, cre)
 end
+
+--- Determine is first level feat only.
+-- @param feat FEAT\_*
+function M.GetFeatIsFirstLevelOnly(feat)
+   local f = C.nwn_GetFeat(feat)
+   if f == nil then return false end
+   return f.feat_max_level == 1
+end
+
+--- Get feat name.
+-- @param feat FEAT\_*
+function M.GetFeatName(feat)
+   local f = C.nwn_GetFeat(feat)
+   if f == nil then return "" end
+   return TLK.GetString(f.feat_name_strref)
+end
+
+--- Determine if feat is class general feat.
+-- @param feat FEAT\_*
+-- @param class CLASS\_TYPE\_*
+function M.GetIsClassGeneralFeat(feat, class)
+   return C.nwn_GetIsClassGeneralFeat(class, feat)
+end
+--- Determine if feat is class bonus feat.
+-- @param feat FEAT\_*
+-- @param class CLASS\_TYPE\_*
+function M.GetIsClassBonusFeat(feat, class)
+   return C.nwn_GetIsClassBonusFeat(class, feat)
+end
+
+--- Determine if feat is class granted feat.
+-- @param feat FEAT\_*
+-- @param class CLASS\_TYPE\_*
+function M.GetIsClassGrantedFeat(feat, class)
+   return C.nwn_GetIsClassGrantedFeat(class, feat)
+end
