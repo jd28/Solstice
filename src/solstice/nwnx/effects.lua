a8d6c30a7033ba569eb92b68e1d79726b9ee02bd
jmd <jmd2028@gmail.com>
2015-01-14 04:55:44 -0800

    nwnx.effects: Add some documentation.

--- src/solstice/nwnx/effects.lua
+++ src/solstice/nwnx/effects.lua
@@ -1,4 +1,20 @@
---- NWNX Effects
+----
+-- Defines the interface to the nwnx_effects plugin.
+--
+-- nwnx_effects essentially hijacks the EFFECT_TYPE_MODIFYNUMATTACKS
+-- The custom type **must** always be stored at index 0 of an effects integer
+-- list.
+--
+-- When an effect, `eff`, of EFFECT_TYPE_MODIFYNUMATTACKS is applied.
+-- An effect handler is looked up by eff:GetInt(0), if none is present
+-- the effect is deleted.  If one is present the handler will be called
+-- with 3 parameters: the effect, an object,
+-- and boolean value indicating whether the effect is being applied
+-- (false) or removed (true).
+--
+-- Any additional information (integers, floats, or strings) can
+-- be stored at any index, except intger index 0.  See Effect:SetInt,
+-- Effect:SetFloat, Effect:SetString.
 -- @module nwnx.effects
 
 require 'solstice.nwn.ctypes.effect'
@@ -26,16 +42,12 @@ function M.GetIsEffectHandlerRegistered(eff_type)
 end
 
 --- Register an effect handler.
--- @param handler Function to call on object when effect is applied or removed.
--- It will be called with three parameters: a RAW effect, an object, and boolean value
--- indicating whether the effect is being applied (false) or removed (true).
--- If the effect cannot applied, say the target is dead, then the function should
--- return true to delete the effect, returning false explicitly is not necessary.
--- @param ... List of CUSTOM\_EFFECT\_TYPE\_*.
+-- @function handler Function to call on object when effect is applied or removed.
+-- @param ... List of custom effect constants.
 function M.RegisterEffectHandler(handler, ...)
    local types = {...}
    if #types == 0 then
-      print(debug.traceback())
+      print("Error: No effects were specified!\n" .. debug.traceback())
    end
    for _, v in ipairs(types) do
       EFF_HANDLERS[v] = handler
@@ -48,10 +60,10 @@ function NWNXEffects_HandleEffectEvent()
 
    local obj = _SOL_GET_CACHED_OBJECT(ev.obj.obj_id)
 
-   local h = EFF_HANDLERS[ev.eff.eff_integers[1]]
+   local h = EFF_HANDLERS[ev.eff.eff_integers[0]]
    if not h then return 0 end
 
-   local del = h(ev.eff, obj, ev.is_remove)
+   local del = h(Eff.effect_t(ev.eff, true), obj, ev.is_remove)
    ev.delete_eff = del and 1 or 0
 
    return 1
