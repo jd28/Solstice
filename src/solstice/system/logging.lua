23fa40345903ed47652c32714baecd36ef6ddb95
jmd <jmd2028@gmail.com>
2015-01-15 02:47:06 -0800

    Redo file logging.

--- src/solstice/system/logging.lua
+++ src/solstice/system/logging.lua
@@ -2,44 +2,57 @@
 -- @module system
 -- @alias M
 
-local C = require('ffi').C
+local ffi = require 'ffi'
+local C = ffi.C
+
 require 'logging'
-local NWE = require 'solstice.nwn.engine'
+
+-- Had to reimpliment the lualogging file logger, since it doesn't
+-- allow controlling the date pattern in the actual log message.
+
+local lastFileNameDatePattern
+local lastFileHandler
+
+local function openFileLogger(filename, datePattern)
+	if (lastFileNameDatePattern ~= filename) then
+		local f = io.open(filename, "a")
+		if (f) then
+			f:setvbuf ("line")
+			lastFileNameDatePattern = filename
+			lastFileHandler = f
+			return f
+		else
+			return nil, string.format("file `%s' could not be opened for writing", filename)
+		end
+	else
+		return lastFileHandler
+	end
+end
+
+local function file(filename, datePattern, logPattern)
+	if type(filename) ~= "string" then
+		filename = "lualogging.log"
+	end
+
+	return logging.new( function(self, level, message)
+		local f, msg = openFileLogger(filename, datePattern)
+		if not f then
+			return nil, msg
+		end
+		local s = logging.prepareLogMsg(logPattern, os.date(datePattern), level, message)
+		f:write(s)
+		return true
+	end)
+end
+
+ffi.cdef[[
+void           Local_NWNXLog(int32_t level, const char* log);
+]]
 
 local M = require 'solstice.system.init'
+M.FileLog = file(OPT.LOG_FILE, OPT.LOG_DATE_PATTERN)
 
---- Write string to NWN server log file
--- @param log String to log.
-local function WriteTimestampedLogEntry(log)
-   NWE.StackPushString(log)
-   NWE.ExecuteCommand(560, 1)
-end
-
-M.WriteTimestampedLogEntry = WriteTimestampedLogEntry
-
---- Write string to nwnx_solstice log file
--- @param log String to log.
-local function WriteLogEntryToNWNX(log)
-   C.Local_NWNXLog(0, log)
-end
-
-M.WriteLogEntryToNWNX = WriteLogEntryToNWNX
-
---- NWN Log.
-M.NWLog = logging.new(
-   function(self, level, message)
-      WriteTimestampedLogEntry(message)
-      return true
-   end)
-
---- nwnx_solstice log.
-M.NXLog = logging.new(
-   function(self, level, message)
-      WriteLogEntryToNWNX(message)
-      return true
-   end)
-
-local _LOGGER = M.NXLog
+local _LOGGER = M.FileLog
 
 --- Sets the system logging function.
 -- This defaults to printing to the nwnx_solstice.txt log file.
