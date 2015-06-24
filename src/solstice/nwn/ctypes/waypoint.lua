ef2585536cf16bd9e2f2bfe6b521cfdbd27226b9
jmd <jmd2028@gmail.com>
2015-06-24 12:58:38 -0700

    Fill in Waypoint ctype.

--- src/solstice/nwn/ctypes/waypoint.lua
+++ src/solstice/nwn/ctypes/waypoint.lua
@@ -2,6 +2,16 @@ local ffi = require 'ffi'
 
 ffi.cdef[[
 typedef struct {
-    void               *unknown;
+    CNWSObject          obj;
+    uint8_t             way_mapnote_has;
+    uint8_t             field_1C5;
+    uint8_t             field_1C6;
+    uint8_t             field_1C7;
+    uint8_t             way_mapnote_enabled;
+    uint8_t             field_1C9;
+    uint8_t             field_1CA;
+    uint8_t             field_1CB;
+    CExoLocString       way_mapnote;
+    CExoLocString       way_name;
 } CNWSWaypoint;
 ]]
