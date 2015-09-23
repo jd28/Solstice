bbf16437ca60870d293ca09468637bdf06b13994
jmd <jmd2028@gmail.com>
2015-09-23 01:21:35 -0700

    Redo situations API

--- src/solstice/rules/situations.lua
+++ src/solstice/rules/situations.lua
@@ -1,156 +1,125 @@
 --- Rules
 -- @module rules
 
-local function ZeroSituationMod(cre, situ)
-   cre.ci.mod_situ[situ].ab = 0
-   cre.ci.mod_situ[situ].ac = 0
-   cre.ci.mod_situ[situ].hp = 0
-   cre.ci.mod_situ[situ].dmg.roll.dice = 0
-   cre.ci.mod_situ[situ].dmg.roll.sides = 0
-   cre.ci.mod_situ[situ].dmg.roll.bonus = 0
-   cre.ci.mod_situ[situ].dmg.type = 12
-end
+local function sneak_attack(modifier, cre)
+  if modifier ~= ATTACK_MODIFIER_DAMAGE then return end
+  local feat
+  local normal = false
+  local dmg = damage_roll_t()
+  dmg.type = 12
+  dmg.roll.sides = 6
 
-local function coupdegrace(cre)
-   ZeroSituationMod(cre, 0)
-end
+  -- Normal sneak.
+  feat = cre:GetHighestFeatInRange(FEAT_SNEAK_ATTACK_11,
+                        FEAT_SNEAK_ATTACK_20)
 
-local function sneak_attack(cre)
-   ZeroSituationMod(cre, 1)
+  if feat ~= -1 then
+    dmg.roll.dice = feat - FEAT_SNEAK_ATTACK_11 + 11
+    normal = true
+  end
 
-   local feat
-   local normal = false
+  if not normal then
+    feat = cre:GetHighestFeatInRange(FEAT_SNEAK_ATTACK_2,
+                          FEAT_SNEAK_ATTACK_10)
 
-   -- Normal sneak.
-   feat = cre:GetHighestFeatInRange(FEAT_SNEAK_ATTACK_11,
-                                    FEAT_SNEAK_ATTACK_20)
-
-   if feat ~= -1 then
-      cre.ci.mod_situ[1].dmg.roll.dice = feat - FEAT_SNEAK_ATTACK_11 + 11
-      cre.ci.mod_situ[1].dmg.roll.sides = 6
+    if feat ~= -1 then
+      dmg.roll.dice = feat - FEAT_SNEAK_ATTACK_2 + 2
       normal = true
-   end
+    end
+  end
 
-   if not normal then
-      feat = cre:GetHighestFeatInRange(FEAT_SNEAK_ATTACK_2,
-                                       FEAT_SNEAK_ATTACK_10)
+  if not normal and cre:GetHasFeat(FEAT_SNEAK_ATTACK_1) then
+    dmg.roll.dice = 1
+  end
 
-      if feat ~= -1 then
-         cre.ci.mod_situ[1].dmg.roll.dice = feat - FEAT_SNEAK_ATTACK_2 + 2
-         cre.ci.mod_situ[1].dmg.roll.sides = 6
-         normal = true
-      end
-   end
+  local bg = false
 
-   if not normal and cre:GetHasFeat(FEAT_SNEAK_ATTACK_1) then
-      cre.ci.mod_situ[1].dmg.roll.dice = 1
-      cre.ci.mod_situ[1].dmg.roll.sides = 6
-   end
+  feat = cre:GetHighestFeatInRange(FEAT_BLACKGUARD_SNEAK_ATTACK_4D6,
+                        FEAT_BLACKGUARD_SNEAK_ATTACK_15D6)
 
-   local bg = false
+  if feat ~= -1 then
+    local add = feat - FEAT_BLACKGUARD_SNEAK_ATTACK_4D6 + 4
+    dmg.roll.dice = dmg.roll.dice + add
+    bg = true
+  end
 
-   feat = cre:GetHighestFeatInRange(FEAT_BLACKGUARD_SNEAK_ATTACK_4D6,
-                                    FEAT_BLACKGUARD_SNEAK_ATTACK_15D6)
-
-   if feat ~= -1 then
-      local add = feat - FEAT_BLACKGUARD_SNEAK_ATTACK_4D6 + 4
-      cre.ci.mod_situ[1].dmg.roll.dice = cre.ci.mod_situ[1].dmg.roll.dice + add
-      cre.ci.mod_situ[1].dmg.roll.sides = 6
+  if not bg then
+    feat = cre:GetHighestFeatInRange(FEAT_BLACKGUARD_SNEAK_ATTACK_1D6,
+                          FEAT_BLACKGUARD_SNEAK_ATTACK_3D6)
+    if feat ~= -1 then
+      local add = feat - FEAT_BLACKGUARD_SNEAK_ATTACK_1D6 + 1
+      dmg.roll.dice = dmg.roll.dice + add
       bg = true
-   end
+    end
+  end
 
-   if not bg then
-      feat = cre:GetHighestFeatInRange(FEAT_BLACKGUARD_SNEAK_ATTACK_1D6,
-                                       FEAT_BLACKGUARD_SNEAK_ATTACK_3D6)
-      if feat ~= -1 then
-         local add = feat - FEAT_BLACKGUARD_SNEAK_ATTACK_1D6 + 1
-         cre.ci.mod_situ[1].dmg.roll.dice = cre.ci.mod_situ[1].dmg.roll.dice + add
-         cre.ci.mod_situ[1].dmg.roll.sides = 6
-         bg = true
-      end
-   end
-
-   -- Improved Sneak Attack
-   feat = cre:GetHighestFeatInRange(FEAT_EPIC_IMPROVED_SNEAK_ATTACK_1,
-                                    FEAT_EPIC_IMPROVED_SNEAK_ATTACK_10)
-   if feat ~= -1 then
-      local add = feat - FEAT_EPIC_IMPROVED_SNEAK_ATTACK_1 + 1
-      cre.ci.mod_situ[1].dmg.roll.dice = cre.ci.mod_situ[1].dmg.roll.dice + add
-      cre.ci.mod_situ[1].dmg.roll.sides = 6
-   end
+  -- Improved Sneak Attack
+  feat = cre:GetHighestFeatInRange(FEAT_EPIC_IMPROVED_SNEAK_ATTACK_1,
+                        FEAT_EPIC_IMPROVED_SNEAK_ATTACK_10)
+  if feat ~= -1 then
+    local add = feat - FEAT_EPIC_IMPROVED_SNEAK_ATTACK_1 + 1
+    dmg.roll.dice = dmg.roll.dice + add
+    normal = true
+  end
+  if normal or bg then return dmg end
 end
 
-local function death_attack(cre)
-   ZeroSituationMod(cre, 2)
-   local death = false
-   local feat = cre:GetHighestFeatInRange(FEAT_PRESTIGE_DEATH_ATTACK_9,
-                                          FEAT_PRESTIGE_DEATH_ATTACK_20)
-   if feat ~= -1 then
-      cre.ci.mod_situ[2].dmg.roll.dice = feat - FEAT_PRESTIGE_DEATH_ATTACK_9 + 9
-      cre.ci.mod_situ[2].dmg.roll.sides = 6
+local function death_attack(modifier, cre)
+  if modifier ~= ATTACK_MODIFIER_DAMAGE then return end
+
+  local dmg = damage_roll_t()
+  dmg.type = 12
+  dmg.roll.sides = 6
+  local death = false
+
+  local feat = cre:GetHighestFeatInRange(FEAT_PRESTIGE_DEATH_ATTACK_9,
+                            FEAT_PRESTIGE_DEATH_ATTACK_20)
+  if feat ~= -1 then
+    dmg.roll.dice = feat - FEAT_PRESTIGE_DEATH_ATTACK_9 + 9
+    death = true
+  end
+
+  if not death then
+    feat = cre:GetHighestFeatInRange(FEAT_PRESTIGE_DEATH_ATTACK_6,
+                          FEAT_PRESTIGE_DEATH_ATTACK_8)
+
+    if feat ~= -1 then
+      dmg.roll.dice = feat - FEAT_PRESTIGE_DEATH_ATTACK_6 + 6
       death = true
-   end
+    end
+  end
 
-   if not death then
-      feat = cre:GetHighestFeatInRange(FEAT_PRESTIGE_DEATH_ATTACK_6,
-                                       FEAT_PRESTIGE_DEATH_ATTACK_8)
+  if not death then
+    feat = cre:GetHighestFeatInRange(FEAT_PRESTIGE_DEATH_ATTACK_1,
+                          FEAT_PRESTIGE_DEATH_ATTACK_5)
 
-      if feat ~= -1 then
-         cre.ci.mod_situ[2].dmg.roll.dice = feat - FEAT_PRESTIGE_DEATH_ATTACK_6 + 6
-         cre.ci.mod_situ[2].dmg.roll.sides = 6
-         death = true
-      end
-   end
-
-   if not death then
-      feat = cre:GetHighestFeatInRange(FEAT_PRESTIGE_DEATH_ATTACK_1,
-                                       FEAT_PRESTIGE_DEATH_ATTACK_5)
-
-      if feat ~= -1 then
-         cre.ci.mod_situ[2].dmg.roll.dice = feat - FEAT_PRESTIGE_DEATH_ATTACK_1 + 1
-         cre.ci.mod_situ[2].dmg.roll.sides = 6
-      end
-   end
+    if feat ~= -1 then
+      dmg.roll.dice = feat - FEAT_PRESTIGE_DEATH_ATTACK_1 + 1
+      death = true
+    end
+  end
+  if death then return dmg end
 end
 
 local _SITU_MOD = {
-   [0] = coupdegrace,
-   sneak_attack,
-   death_attack,
+  [0] = coupdegrace,
+  sneak_attack,
+  death_attack,
 }
 
 --- Situation Modifiers
 -- @section
 
+local function GetSituationModifier(situ, modifier, cre)
+  if not _SITU_MOD[situation] then return end
+  return _SITU_MOD[situation](modifier, cre)
+end
+
 --- Override Situation Modifier.
--- @param situation SITUATION_* see situations.2da
--- @param func A function taking a creature paramater
--- that calculates the combat modifier for a situation
--- at a given index.
-local function SetSituationModiferOverride(situation, func)
-   if situation < 0 or situation >= SITUATION_NUM then
-      error "Invalid situation constant"
-   end
-   _SITU_MOD[situation] = func
-end
-
---- Resolves situation modifier.
--- @param type SITUATION_* see situations.2da
--- @param cre Creature object.
-local function ResolveSituationModifier(type, cre)
-   local f = _SITU_MOD[type]
-   if f then f(cre) else error "Invalid situation mod function" end
-end
-
---- Resolves all situation modifiers
--- @param cre Creature object
-local function ResolveSituationModifiers(cre)
-   for i=0, SITUATION_NUM - 1 do
-      ResolveSituationModifier(i, cre)
-   end
+local function RegisterSituation(situation, func)
+  _SITU_MOD[situation] = func
 end
 
 local M = require 'solstice.rules.init'
-M.SetSituationModiferOverride = SetSituationModiferOverride
-M.ResolveSituationModifier    = ResolveSituationModifier
-M.ResolveSituationModifiers   = ResolveSituationModifiers
+M.GetSituationModifier = GetSituationModifier
+M.RegisterSituation = RegisterSituation
