b71d19b6f805b897381f9aa48a5442082ad9b95b
jmd <jmd2028@gmail.com>
2015-09-28 02:06:53 -0700

    Redo GetAbilityEffectModifier to behave like other effect modifier functions.

--- docs/rules/abilities.rst
+++ docs/rules/abilities.rst
@@ -13,9 +13,9 @@ Abilities
   :param int ability: ABILITY_*
   :rtype: string
 
-.. function:: GetAbilityEffectLimits(cre, ability)
+.. function:: GetAbilityEffectLimits([cre[, ability]])
 
-  Get the limits of ability effects
+  Get the limits of ability effects.  Both parameters are optional, the are there merely to facilitate customizing effect limits by ability or creature, supposing someone wanted to do that.
 
   :param cre: Creature instance
   :type cre: :class:`Creature`
@@ -24,14 +24,15 @@ Abilities
   :rtype: int
 
 
-.. function:: GetAbilityEffectModifier(cre, ability)
+.. function:: GetAbilityEffectModifier(cre[, ability])
 
-  Get ability modification from effects.
+  Get ability modification from effects.  The return value is not clamped or modified by :func:`GetAbilityEffectLimits`.
 
   .. warning::
+
     This currently does not provide default behavior.  Everything stacks: items, spells, etc.
 
   :param cre: Creature instance
   :type cre: :class:`Creature`
   :param int ability: ABILITY_*
-  :rtype: int
+  :rtype: If the ``ability`` parameter is not passed an array of all ability effect modifiers of length ``ABILITY_NUM`` is returned.  Note this array is static and should not be modified or stored by callers.  If the ``ability`` parameter is passed only that ability effect modifier is returned.
--- src/solstice/rules/ability.lua
+++ src/solstice/rules/ability.lua
@@ -4,6 +4,8 @@
 --- Abilities
 -- @section ability
 
+local ffi = require 'ffi'
+
 local function GetAbilityName(ability)
    return Game.GetTlkString(TDA.GetInt("abilities", "Name", ability))
 end
@@ -21,26 +23,37 @@ end
 -- @param cre Creature object.
 -- @param ability ABILITY_*
 -- @return Total amount, uncapped
+local ABILITY_EFF = ffi.new('int32_t[?]', ABILITY_NUM)
 local function GetAbilityEffectModifier(cre, ability)
-   local eff = 0
-   if cre.obj.obj.obj_effects_len <= 0 then return eff end
-   if cre.obj.cre_stats.cs_first_ability_eff > 0 then
-      for i = cre.obj.cre_stats.cs_first_ability_eff, cre.obj.obj.obj_effects_len - 1 do
-         if cre.obj.obj.obj_effects[i].eff_type ~= EFFECT_TYPE_ABILITY_INCREASE
-            and cre.obj.obj.obj_effects[i].eff_type ~= EFFECT_TYPE_ABILITY_DECREASE
-         then
-            break
-         end
-         if cre.obj.obj.obj_effects[i].eff_integers[0] == ability then
-            if cre.obj.obj.obj_effects[i].eff_type == EFFECT_TYPE_ABILITY_INCREASE then
-               eff = eff + cre.obj.obj.obj_effects[i].eff_integers[1]
-            else
-               eff = eff - cre.obj.obj.obj_effects[i].eff_integers[1]
-            end
-         end
+  for i=0, ABILITY_NUM -1 do
+     ABILITY_EFF[i] = 0
+  end
+
+  if cre.obj.obj.obj_effects_len <= 0 then return 0 end
+  if cre.obj.cre_stats.cs_first_ability_eff > 0 then
+    for i = cre.obj.cre_stats.cs_first_ability_eff, cre.obj.obj.obj_effects_len - 1 do
+      if cre.obj.obj.obj_effects[i].eff_type ~= EFFECT_TYPE_ABILITY_INCREASE
+        and cre.obj.obj.obj_effects[i].eff_type ~= EFFECT_TYPE_ABILITY_DECREASE
+      then
+        break
       end
-   end
-   return eff
+
+      local abtype = cre.obj.obj.obj_effects[i].eff_integers[0]
+      local amt = cre.obj.obj.obj_effects[i].eff_integers[1]
+
+      if cre.obj.obj.obj_effects[i].eff_type == EFFECT_TYPE_ABILITY_INCREASE then
+        ABILITY_EFF[i] = ABILITY_EFF[i] + amt
+      else
+        ABILITY_EFF[i] = ABILITY_EFF[i] - amt
+      end
+    end
+  end
+
+  if not ability then
+    return ABILITY_EFF[ability]
+  else
+    return ABILITY_EFF
+  end
 end
 
 local M = require 'solstice.rules.init'
